f1344d7e01bf91648b95dcf3a0b9a11164523364
marcinfo <M@rcela20>
2022-12-17 11:18:17 -0300

    mensagens

--- vacinapaulistana/vacina/views.py
+++ vacinapaulistana/vacina/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from django.contrib import messages
+from .forms import LoginForm, UserRegistrationForm,UserEditForm,ProfileEditForm
+from .models import Profile
 
 
 def user_login(request):
@@ -38,7 +40,8 @@ def register(request):
                 user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
-
+            # Create the user profile
+            Profile.objects.create(user=new_user)
             return render(request,
                           'vacina/register_done.html',
                           {'new_user': new_user})
@@ -52,3 +55,26 @@ def register(request):
 @login_required
 def dashboard(request):
     return render(request, 'vacina/dashboard.html', {'section': 'dashboard'})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile,
+                                    data=request.POST,
+                                    files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Atualizado com sucesso!')
+        else:
+            messages.error(request, 'Erro ao atualizar!')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request,
+                  'vacina/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
